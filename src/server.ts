227c34fbd094f167fa78d51b4ebd5611e1ce585a
Shashank Daima <daimashashank10@gmail.com>
2024-03-19 01:58:45 +0530

    minor experimentation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,3 +9,23 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
+// const prisma = new PrismaClient();
+
+
+// async function main() {
+//   const newUser = await prisma.user.create({
+//     data: {
+//       name: 'Alice',
+//       email: 'alice@prisma.io',
+//     },
+//   })
+//   console.log(newUser)
+// }
+
+// main()
+//   .catch((e) => {
+//     throw e
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
