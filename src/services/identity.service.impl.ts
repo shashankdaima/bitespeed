6844105be22e4d77f0fa47d92f821f20390101de
Shashank Daima <daimashashank10@gmail.com>
2024-03-19 05:09:17 +0530

    minor.

--- src/services/identity.service.impl.ts
+++ src/services/identity.service.impl.ts
@@ -1,9 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import { IdentityResponse } from "../models/identity.post.response";
 import { Exception, Result } from "../utils/result.util";
 import { IdentityService } from "./identity.service";
-
+const prisma = new PrismaClient();
 export class IdentityServiceImpl implements IdentityService {
-    identifyUser(email: string, phoneNumber: string): Result<IdentityResponse> {
+    async identifyUser(email: string, phoneNumber: string): Promise<Result<IdentityResponse>>  {
+        // prisma.contact.
         return new Exception("Method not implemented.");
     }
 }
--- src/services/identity.service.ts
+++ src/services/identity.service.ts
@@ -1,5 +1,5 @@
 import { IdentityResponse } from '../models/identity.post.response';
 import{Result, Success, Exception} from '../utils/result.util';
 export interface IdentityService{
-    identifyUser(email:string,phoneNumber:string):Result<IdentityResponse>;
+    identifyUser(email:string,phoneNumber:string):Promise<Result<IdentityResponse>>;
 }
