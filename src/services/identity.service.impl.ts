0324f9be123437a65c3c0e9f1782e053cb35b23c
Shashank Daima <daimashashank10@gmail.com>
2024-03-19 07:00:06 +0530

    unoptimized queries running but malfunctioning

--- src/services/identity.service.impl.ts
+++ src/services/identity.service.impl.ts
@@ -1,11 +1,81 @@
 import { PrismaClient } from "@prisma/client";
 import { IdentityResponse } from "../models/identity.post.response";
-import { Exception, Result } from "../utils/result.util";
+import { Exception, Result, Success } from "../utils/result.util";
 import { IdentityService } from "./identity.service";
 const prisma = new PrismaClient();
 export class IdentityServiceImpl implements IdentityService {
-    async identifyUser(email: string, phoneNumber: string): Promise<Result<IdentityResponse>>  {
-        // prisma.contact.
-        return new Exception("Method not implemented.");
+    async identifyUser(email: string, phoneNumber: string): Promise<Result<IdentityResponse>> {
+
+        try{
+            await prisma.contact.create({
+                data: {
+                    linkPrecedence: "primary",
+                    email: email,
+                    phoneNumber: phoneNumber
+                }
+            });
+        }catch(ignored){
+            //ignored because only constaint that is unique is email and phone number
+        }
+        let mainContact = await prisma.contact.findFirst({
+            where: {
+                OR: [
+                    {
+                        email: email
+                    },
+                    {
+                        phoneNumber: phoneNumber
+                    }
+                ]
+            }, orderBy: { createdAt: "asc" }
+        });
+        if (!mainContact) {
+            return new Exception("Main Contact can't be found", 404);
+
+        }
+        //existing user
+        await prisma.contact.updateMany({
+            where: {
+                AND: [
+                    {
+                        OR: [
+                            { phoneNumber: { equals: phoneNumber } },
+                            { email: { equals: email } }
+                        ]
+                    },
+                    { id: { not: mainContact.id } }
+                ]
+            },
+            data: {
+                linkPrecedence: "secondary",
+                linkedId: mainContact.id
+            }
+        });
+
+
+        // Collect secondary rows
+        const secondaryContacts = await prisma.contact.findMany({
+            where: { linkPrecedence: "secondary", linkedId: mainContact.id }
+        });
+        // Collect emails
+        let emails: string[] = [];
+        let phoneNumbers: string[] = [];
+        let secondaryContactIds: number[] = [];
+        secondaryContacts.forEach(contact => {
+            emails.push(contact.email!);
+            phoneNumbers.push(contact.phoneNumber!);
+            secondaryContactIds.push(contact.id!);
+        });
+
+        return new Success<IdentityResponse>({
+            contact: {
+                primaryContatctId: mainContact.id,
+                emails: emails,
+                phoneNumbers: phoneNumbers,
+                secondaryContactIds: secondaryContactIds,
+            }
+        });
+
     }
+
 }
--- src/utils/result.util.ts
+++ src/utils/result.util.ts
@@ -1,16 +1,18 @@
+import { number } from "zod";
+
 class Result<T> {
-    constructor(public data: T|null, public error: any) {}
+    constructor(public data: T | null, public error: any, public code: number){}
 }
 
 class Success<T> extends Result<T> {
     constructor(public data: T) {
-        super(data, null);
+        super(data, null,200);
     }
 }
 
 class Exception<T> extends Result<T> {
-    constructor(public error: any) {
-        super(null, error);
+    constructor(public error: any, public code: number = 500) {
+        super(null, error, code);
     }
 }
-export {Result, Success, Exception };
+export { Result, Success, Exception };
