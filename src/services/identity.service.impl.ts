c1dafd6076b135789d028353e83ba462e4163cc1
Shashank Daima <daimashashank10@gmail.com>
2024-03-21 22:20:37 +0530

    finally running correctly. with repetitions

--- src/services/identity.service.impl.ts
+++ src/services/identity.service.impl.ts
@@ -12,7 +12,7 @@ export class IdentityServiceImpl implements IdentityService {
                         OR: [
                             { email: email },
                             { phoneNumber: phoneNumber }
-                        ],
+                        ]
                     }
                 });
 
@@ -20,13 +20,20 @@ export class IdentityServiceImpl implements IdentityService {
                 if (previousRefs === 0) {
                     return await createNewCluster();
                 }
-                const possibleClusterMakers = await tx.contact.findMany({
+
+                const exactMatchFound = await tx.contact.count({
+                    where: {
+                        email: email,
+                        phoneNumber: phoneNumber
+                    }
+                });
+                const possibleClusterMakers = await tx.contact.findFirst({
                     where: {
                         OR: [
                             { email: email },
                             { phoneNumber: phoneNumber }
-                        ], 
-                        contactClusterId:{
+                        ],
+                        contactClusterId: {
                             not: null
                         }
                     },
@@ -39,78 +46,101 @@ export class IdentityServiceImpl implements IdentityService {
                         contactCluster: true
                     }
                 });
-                const exactMatchFound = await tx.contact.count({
-                    where: {
-                        email: email,
-                        phoneNumber: phoneNumber
-                    }
-                });
-                if (!exactMatchFound) {
+                console.log("possibleClusterMakers", possibleClusterMakers);
+                const clusterId = possibleClusterMakers?.contactCluster?.id ?? null;
+                console.log("clusterId", clusterId);
+                if (exactMatchFound === 0) {
                     await tx.contact.create({
                         data: {
                             email: email,
                             phoneNumber: phoneNumber,
-                            contactClusterId: null
+                            contactClusterId: clusterId
                         }
                     })
+                    console.log("created new contact");
+
                 }
-                let clusterId:number|undefined = undefined;
-                for (const possibleClusterMaker of possibleClusterMakers) {
-                    if(possibleClusterMaker.id==possibleClusterMaker.contactCluster?.creatorId){
-                        clusterId=possibleClusterMaker.contactCluster?.id;
-                        await tx.contact.updateMany({
-                            where:{
-                                id: {
-                                    not:possibleClusterMaker.id
-                                },
-                                OR: [
-                                    { email: email },
-                                    { phoneNumber: phoneNumber }
-                                ],
-                            },
-                            data:{
-                                contactClusterId: possibleClusterMaker.contactCluster?.id   
-                            }
-                        });
-                        break;
-                    }
-                }
-                const contactCluster=await tx.contactCluster.findFirst({
+                const contactsWithOtherClusters = await tx.contact.findMany({
                     where: {
-                        id: clusterId
+                        OR: [
+                            { email: email },
+                            { phoneNumber: phoneNumber }
+                        ],
+                        contactClusterId: {
+                            not: clusterId
+                        }
+                    },
+
+                });
+                //till now checked
+                for (const contact of contactsWithOtherClusters) {
+                    const otherCluster = await tx.contactCluster.findFirst({
+                        where: {
+                            id: contact.contactClusterId!
+
+                        },
+                        include: {
+                            members: true
+                        }
+                    })
+                    for (const member of otherCluster!.members) {
+                        await tx.contact.update({
+                            where: {
+                                id: member.id
+                            },
+                            data: {
+                                contactClusterId: clusterId
+                            }
+                        })
+                    }
+                    await tx.contactCluster.delete({
+                        where: {
+                            id: otherCluster!.id
+                        }
+                    });
+                }
+                const emails: string[] = [];
+                const phoneNumbers: string[] = [];
+                const secondaryContactIds: number[] = [];
+                const mainCluster = await tx.contactCluster.findFirst({
+                    where: {
+                        id: clusterId!
+
                     },
                     include: {
                         members: true
                     }
-                });
-                const emails: string[] = [];
-                const phoneNumbers: string[] = [];
-                const secondaryContactIds: number[] = [];
-                for (const member of contactCluster?.members??[]) {
+                })
+                for (const member of mainCluster?.members ?? []) {
                     if (member.email) {
                         emails.push(member.email);
                     }
                     if (member.phoneNumber) {
                         phoneNumbers.push(member.phoneNumber);
                     }
-                    if (member.id!==contactCluster?.creatorId ) {
+                    if (member.id !== mainCluster?.creatorId!) {
                         secondaryContactIds.push(member.id);
                     }
                 }
+                console.log("emails", emails);
+                console.log("phoneNumbers", phoneNumbers);
+                console.log("secondaryContactIds", secondaryContactIds);
                 const response = ({
                     contact: {
-                        primaryContactId: contactCluster!.creatorId,
+                        primaryContactId: mainCluster?.creatorId!,
                         emails: emails,
                         phoneNumbers: phoneNumbers,
                         secondaryContactIds: secondaryContactIds
                     }
                 });
+                console.log("response", response);
 
                 return new Success(response);
 
             } catch (e: Error | any) {
                 return new Exception(e?.message ?? "Unknown Error");
             }
+
 
             async function createNewCluster() {
                 const newContact = await tx.contact.create({
